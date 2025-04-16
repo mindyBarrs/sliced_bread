39280e4fb77d8302bc9bad1b5400c0ab905b44af
mindyBarrs <barrs.mindy@gmail.com>
2025-04-16 10:13:11 -0400

    fixed up the description

--- src/app/api/getBeverage.tsx
+++ src/app/api/getBeverage.tsx
@@ -8,13 +8,12 @@ const transformDrinkData = async (data: any): Promise<BeverageResponse> => {
 	return {
 		name: drink.strDrink,
 		imageSrc: drink.strDrinkThumb,
-		description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dictum aliquet est, et viverra dui 
-			egestas ac.Aliquam consequat enim vel tempor maximus.Aliquam luctus eros posuere ligula convallis 
-			molestie.Pellentesque nec diam nisl.Vivamus ultrices turpis velit, sit amet euismod tellus porta in.
-			Etiam malesuada ligula id mauris iaculis vulputate.Praesent finibus elit eget turpis elementum facilisis.
-			Donec ullamcorper sed ligula ac imperdiet.Mauris non purus lorem.Donec ut metus bibendum, imperdiet urna 
-			eu, dignissim erat.Vivamus porttitor sodales arcu quis luctus.Sed posuere porttitor tempor.Vestibulum 
-			vel varius libero, nec porttitor ante.Integer leo justo, finibus et pharetra tempus, finibus nec lacus.`,
+		description: `This drink isn’t just a beverage — it’s an experience. From the first sip, it wraps your 
+		senses in a perfect storm of flavor: bold yet smooth, refreshing but with depth, a harmony of warmth,
+		brightness, and just the right touch of sweetness. It’s the kind of drink that elevates conversations,
+		softens the edges of long days, and turns ordinary moments into something memorable. Whether sipped slowly
+		or enjoyed with friends, it delivers the kind of satisfaction that only comes from something expertly
+		balanced and endlessly enjoyable. It’s not just a drink — it’s the one you always come back to.`,
 	};
 };
 
